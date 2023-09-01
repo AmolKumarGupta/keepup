2a5aceb1eb8d9548445c2c88805e38ccecb60219
amol <amlo1232001@gmail.com>
2023-09-02 00:18:20 +0530

    feat: setup react-router-dom, closes #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { Outlet } from "react-router-dom"
+import Index from './pages/Index'
 
 function App() {
   return (
     <>
       <h1 className='text-red-900'>Vite + React</h1>
+      <Outlet />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import App from './App.jsx'
+import Test from './pages/Test'
+import Index from './pages/Index'
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Index />
+      },
+      {
+        path: "test",
+        element: <Test />
+      },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
