47f0685496e47c26dae1f283ad5c76ebb8adc1e3
amol <amlo1232001@gmail.com>
2023-09-03 15:46:58 +0530

    feat: navbar, closes #6

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "spin-once": "spin 1s linear 1",
+      },
+    },
   },
   plugins: [],
-}
-
+};
