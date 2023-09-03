0f45418ecaa41e669d0052d288b67909aa255c5b
amol <amlo1232001@gmail.com>
2023-09-03 14:47:12 +0530

    feat: .env.example , closes #4

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom"
 function App() {
   return (
     <>
-      <h1 className='text-red-900'></h1>
+      <h1 className='text-red-900'>{ import.meta.env.VITE_APP_NAME }</h1>
       <Outlet />
     </>
   )
