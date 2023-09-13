e55eaa5162cd0946ea42ded97e5d721e759f1a1c
amol <amlo1232001@gmail.com>
2023-09-14 00:14:44 +0530

    feat: option page, closes #28

--- src/pages/Options.jsx
+++ src/pages/Options.jsx
@@ -1,5 +1,29 @@
+import { Link } from "react-router-dom";
+import useAppStore from "../store/appStore";
+
 function Options() {
-  return <></>;
+  const toggleOptionPage = useAppStore((s) => s.toggleOptionPage);
+  const pages = {
+    home: "/",
+    my_test: "test",
+  };
+
+  return (
+    <div
+      onClick={toggleOptionPage}
+      className="my-12 space-y-3 text-xl text-center capitalize pointer"
+    >
+      {Object.entries(pages).map(([name, link]) => {
+        return (
+          <div key={name} className="hover:text-gray-500">
+            <Link to={link} reloadDocument={true}>
+              {name.replace("_", " ")}
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Options;
