ffb21e6ca4edac661ac7ecbe7a7651c07a989944
Logan Wright <loganlwright@gmail.com>
2023-09-13 22:50:46 -0400

    Example styling for button

--- src/pages/Export.jsx
+++ src/pages/Export.jsx
@@ -8,7 +8,11 @@ export default function Export() {
 	return (
 		<>
 			<h1 className="text-xl bold">Export Data</h1>
-			<button type="button" onClick={getLocalStorage}>
+			<button
+				type="button"
+				className="mt-3 px-3 py-1 rounded shadow active:shadow-blue-600 bg-blue-500 hover:bg-blue-600 text-white cursor-pointer"
+				onClick={getLocalStorage}
+			>
 				Click Me!
 			</button>
 		</>
