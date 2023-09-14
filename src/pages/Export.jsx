849f5ca5e46d4cf12fab96b17747cfe0020d3fef
Logan Wright <loganlwright@gmail.com>
2023-09-14 16:27:04 -0400

    Date picker?

--- src/pages/Export.jsx
+++ src/pages/Export.jsx
@@ -1,21 +1,37 @@
+import { prefix } from "../utils";
+import React, { useState } from "react";
+
 export default function Export() {
 	//TODO?
 	//Clean up functionality
+	//Better date picker? or remove entirely?
+
+	const [userInput, setUserInput] = useState("");
+
+	const handleInputChange = (event) => {
+		setUserInput(event.target.value);
+	};
 
 	function getLocalStorage() {
 		//get the data from localstorage
 		const data = Object.keys(localStorage);
-		// if there is no data, return early
+
 		if (!data) {
 			return;
 		}
+
 		//data filtered by the key set in env variable
-		const matchingKey = data.filter((key) =>
-			key.startsWith(import.meta.env.VITE_LS_PREFIX)
+		const matchingKeys = data.filter(
+			(key) =>
+				key === import.meta.env.VITE_LS_PREFIX + userInput.toString()
 		);
 
+		if (matchingKeys.length === 0) {
+			return console.log("No matching keys.");
+		}
+
 		//for each match, get item and create new blob
-		matchingKey.forEach((key) => {
+		matchingKeys.forEach((key) => {
 			const localStorageData = localStorage.getItem(key);
 			const blob = new Blob([localStorageData], {
 				type: "application/json",
@@ -36,8 +52,22 @@ export default function Export() {
 	return (
 		<>
 			<h1 className="text-xl bold">Export Data</h1>
+			<h3 className="">
+				Enter in the date you want to download. Format such as 9/14/2023
+			</h3>
+
+			{/* Input field */}
+			<input
+				type="text"
+				id="textInput"
+				value={userInput}
+				onChange={handleInputChange}
+				placeholder="Enter date as m/dd/yyy"
+				required
+			/>
+
 			<button
-				type="button"
+				type="submit"
 				className="mt-3 px-3 py-1 rounded shadow active:shadow-blue-600 bg-blue-500 hover:bg-blue-600 text-white cursor-pointer"
 				onClick={getLocalStorage}
 			>
