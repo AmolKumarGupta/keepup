eed67de3fce8dda94489a2f0c7c99d532e9f208b
amol <amlo1232001@gmail.com>
2023-09-15 23:50:40 +0530

    feat: remove unused var

--- src/pages/Export.jsx
+++ src/pages/Export.jsx
@@ -1,78 +1,72 @@
-import { prefix } from "../utils";
-import React, { useState } from "react";
+import { useState } from "react";
 
 export default function Export() {
-	//TODO?
-	//Clean up functionality
-	//Better date picker? or remove entirely?
+  /**
+   * TODO:
+   * Clean up functionality,
+   * Better date picker? or remove entirely?
+   */
 
-	const [userInput, setUserInput] = useState("");
+  const [userInput, setUserInput] = useState("");
 
-	const handleInputChange = (event) => {
-		setUserInput(event.target.value);
-	};
+  const handleInputChange = (event) => {
+    setUserInput(event.target.value);
+  };
 
-	function getLocalStorage() {
-		//get the data from localstorage
-		const data = Object.keys(localStorage);
+  function getLocalStorage() {
+    const data = Object.keys(localStorage);
 
-		if (!data) {
-			return;
-		}
+    if (!data) {
+      return;
+    }
 
-		//data filtered by the key set in env variable
-		const matchingKeys = data.filter(
-			(key) =>
-				key === import.meta.env.VITE_LS_PREFIX + userInput.toString()
-		);
+    const matchingKeys = data.filter(
+      (key) => key === import.meta.env.VITE_LS_PREFIX + userInput.toString()
+    );
 
-		if (matchingKeys.length === 0) {
-			return console.log("No matching keys.");
-		}
+    if (matchingKeys.length === 0) {
+      return alert("No matching keys.");
+    }
 
-		//for each match, get item and create new blob
-		matchingKeys.forEach((key) => {
-			const localStorageData = localStorage.getItem(key);
-			const blob = new Blob([localStorageData], {
-				type: "application/json",
-			});
-			const blobURL = URL.createObjectURL(blob);
-			const a = document.createElement("a"); //create element to download file
-			a.style.display = "none";
-			a.href = blobURL;
-			a.download = "exported_data.json"; //file name exported as
-			document.body.appendChild(a);
-			a.click();
-			// Clean up
-			URL.revokeObjectURL(blobURL);
-			document.body.removeChild(a);
-		});
-	}
+    matchingKeys.forEach((key) => {
+      const localStorageData = localStorage.getItem(key);
+      const blob = new Blob([localStorageData], {
+        type: "application/json",
+      });
+      const blobURL = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.style.display = "none";
+      a.href = blobURL;
+      a.download = "keepup.json";
+      document.body.appendChild(a);
+      a.click();
 
-	return (
-		<>
-			<h1 className="text-xl bold">Export Data</h1>
-			<h3 className="">
-				Enter in the date you want to download. Format such as 9/14/2023
-			</h3>
+      URL.revokeObjectURL(blobURL);
+      document.body.removeChild(a);
+    });
+  }
 
-			{/* Input field */}
-			<input
-				type="text"
-				id="textInput"
-				value={userInput}
-				onChange={handleInputChange}
-				placeholder="Enter date as m/dd/yyy"
-				required
-			/>
+  return (
+    <>
+      <h1 className="text-xl bold">Export Data</h1>
+      <h3 className="">Enter date</h3>
 
-			<button
-				type="submit"
-				className="mt-3 px-3 py-1 rounded shadow active:shadow-blue-600 bg-blue-500 hover:bg-blue-600 text-white cursor-pointer"
-				onClick={getLocalStorage}
-			>
-				Download Data as .JSON file
-			</button>
-		</>
-	);
+      <input
+        type="text"
+        id="textInput"
+        value={userInput}
+        onChange={handleInputChange}
+        placeholder="Ex: 14/09/2023"
+        required
+      />
+
+      <button
+        type="submit"
+        className="mt-3 px-3 py-1 rounded shadow active:shadow-blue-600 bg-blue-500 hover:bg-blue-600 text-white cursor-pointer"
+        onClick={getLocalStorage}
+      >
+        Download
+      </button>
+    </>
+  );
 }
