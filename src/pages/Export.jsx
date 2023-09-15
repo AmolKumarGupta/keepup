de49d9974737b9e43e050e634e231f97a6a68e55
amol <amlo1232001@gmail.com>
2023-09-16 01:24:15 +0530

    feat: export all, closes #33

--- src/pages/Export.jsx
+++ src/pages/Export.jsx
@@ -1,17 +1,34 @@
 import { useState } from "react";
 
 export default function Export() {
-  /**
-   * TODO:
-   * Clean up functionality,
-   * Better date picker? or remove entirely?
-   */
-
   const [userInput, setUserInput] = useState("");
 
   const handleInputChange = (event) => {
     setUserInput(event.target.value);
   };
+
+  function exportAll() {
+    const data = Object.keys(localStorage);
+
+    if (!data) {
+      return alert("No data found!");
+    }
+
+    const matchingKeys = data.filter((key) =>
+      key.startsWith(import.meta.env.VITE_LS_PREFIX)
+    );
+
+    if (matchingKeys.length === 0) {
+      return alert("No data found!");
+    }
+
+    let filteredData = {};
+
+    matchingKeys.forEach((key) => {
+      filteredData[key] = JSON.parse(localStorage.getItem(key));
+    });
+    download([JSON.stringify(filteredData)]);
+  }
 
   function getLocalStorage() {
     const data = Object.keys(localStorage);
@@ -29,31 +46,42 @@ export default function Export() {
     }
 
     matchingKeys.forEach((key) => {
-      const localStorageData = localStorage.getItem(key);
-      const blob = new Blob([localStorageData], {
-        type: "application/json",
-      });
-      const blobURL = URL.createObjectURL(blob);
-      const a = document.createElement("a");
-      a.style.display = "none";
-      a.href = blobURL;
-      a.download = "keepup.json";
-      document.body.appendChild(a);
-      a.click();
-
-      URL.revokeObjectURL(blobURL);
-      document.body.removeChild(a);
+      const data = localStorage.getItem(key);
+      download([data], key);
     });
+  }
+
+  function download(data, label = "") {
+    const blob = new Blob(data, { type: "text/plain" });
+    const blobURL = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.style.display = "none";
+    a.href = blobURL;
+    a.download = label ? `keepup-${label}.json` : `keepup.json`;
+    document.body.appendChild(a);
+    a.click();
+
+    URL.revokeObjectURL(blobURL);
+    document.body.removeChild(a);
   }
 
   return (
     <>
       <h1 className="text-lg bold mb-4">Export Data</h1>
 
-      <section className="text-center">
+      <section className="text-center space-y-3">
+        <div>
+          <button
+            type="button"
+            className="px-3 py-1 min-w-[160px] rounded shadow active:shadow-blue-600 bg-blue-500 hover:bg-blue-600 text-white cursor-pointer"
+            onClick={exportAll}
+          >
+            Export All
+          </button>
+        </div>
+
         <div className="flex justify-center ">
           <input
-            type="text"
             id="textInput"
             className="px-3 py-1 max-w-[160px] rounded rounded-r-none shadow active:shadow-blue-600 cursor-pointer"
             value={userInput}
