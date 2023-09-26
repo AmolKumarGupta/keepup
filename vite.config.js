eaf0584ad0dd87a8918dbc60bcece22ff32fb6f7
amol <amlo1232001@gmail.com>
2023-09-26 11:09:49 +0530

    feat: pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,52 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+const manifestForPlugIn = {
+  registerType: "prompt",
+  includeAssets: ["favicon.ico", "apple-touch-icon.png", "maskable_icon.png"],
+  manifest: {
+    name: "Keepup",
+    short_name: "Keepup",
+    description:
+      "Keepup - Habit Tracker Progressive Web AppI am a simple vite app",
+    icons: [
+      {
+        src: "android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+      {
+        src: "maskable_icon.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+    theme_color: "#209CEE",
+    background_color: "#ffffff",
+    display: "standalone",
+    scope: "/keepup/",
+    start_url: "/keepup/",
+    orientation: "portrait",
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/keepup/",
+  plugins: [react(), VitePWA(manifestForPlugIn)],
+});
